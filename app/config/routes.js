2ca73a559efd98ca8de0cc07309115814d12177a
caigua <caigua@huanleguang.com>
2017-03-06 00:11:06 +0800

    线条基础

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,8 @@
 
 import NotFound from '../pages/index/404';
 import Index from '../pages/index/index';
+import BasicShapesIndex from '../pages/basic-shapes/index';
+import Lines001 from '../pages/basic-shapes/lines/001';
 
 export default [
     {
@@ -12,6 +14,14 @@ export default [
     },{
         path: '/index',
         component: Index
+    },
+
+    {
+        path: '/basic-shapes',
+        component: BasicShapesIndex
+    },{
+        path: '/basic-shapes/lines/001',
+        component: Lines001
     },
 
     {
