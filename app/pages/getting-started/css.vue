b66c8c2c82975e5900a9021fe2bfd08b532c7fb6
caigua <caigua@huanleguang.com>
2017-03-08 02:30:39 +0800

    svg引入方式 - css

---
+++ app/pages/getting-started/css.vue
@@ -0,0 +1,29 @@
+<style lang="less" scoped>
+    .bgi {
+        width: 96px;
+        height: 96px;
+        background-image: url("/app/images/watermelon.svg");
+        background-size: 100% 100%;
+    }
+</style>
+
+<template>
+    <main>
+        <ul>
+            <li>
+                <h1>background-image</h1>
+                <p class="bgi"></p>
+            </li>
+            <li>
+                <h1>list-image</h1>
+            </li>
+            <li>
+                <h1>border-image</h1>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    module.exports = {};
+</script>
