f44764bea9706f6fba6d890f3296f2044572921d
caigua <caigua@huanleguang.com>
2017-08-23 10:42:19 +0800

    Basic-Shapes rect

--- app/pages/basic-shapes/rectangles/001.vue
+++ app/pages/basic-shapes/rectangles/001.vue
@@ -5,19 +5,28 @@
 <template>
     <main>
         <ul>
+            <!-- 默认用黑色填充 -->
+            <li>
+                <svg xmlns="http://www.w3.org/2000/svg" width="400" height="300" viewBox="0 0 400 300">
+                    <rect x="100" y="75" width="200" height="150" rx="0" ry="0" stroke-width="5" stroke="#000" />
+                </svg>
+            </li>
+            <!-- 不填充，保持透明 -->
             <li>
                 <svg xmlns="http://www.w3.org/2000/svg" width="400" height="300" viewBox="0 0 400 300">
                     <rect x="100" y="75" width="200" height="150" rx="0" ry="0" stroke-width="5" stroke="#000" fill="none" />
                 </svg>
             </li>
+            <!-- 指定填充颜色 -->
             <li>
                 <svg xmlns="http://www.w3.org/2000/svg" width="400" height="300" viewBox="0 0 400 300">
-                    <rect x="100" y="75" width="200" height="150" rx="0" ry="0" stroke-width="5" stroke="#000" fill="#007dd4" />
+                    <rect x="100" y="75" width="200" height="150" stroke-width="5" stroke="#000" fill="#007dd4" />
                 </svg>
             </li>
+            <!-- 半透明填充 fill-opacity -->
             <li>
                 <svg xmlns="http://www.w3.org/2000/svg" width="400" height="300" viewBox="0 0 400 300">
-                    <rect x="100" y="75" width="200" height="150" rx="20" ry="40" stroke-width="5" stroke="#000" fill="#007dd4" />
+                    <rect x="100" y="75" width="200" height="150" stroke-width="5" stroke="#c7254e" fill="#007dd4" fill-opacity=".5" />
                 </svg>
             </li>
         </ul>
