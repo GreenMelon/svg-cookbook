a9bb2370c9f9546f45fc771c4e061306a80ad3a8
caigua <caigua@huanleguang.com>
2017-08-24 19:03:18 +0800

    Transform: translate

---
+++ app/pages/transform-coordinate/translate/001.vue
@@ -0,0 +1,21 @@
+<style lang="less" scoped>
+</style>
+
+<template>
+    <main>
+        <svg xmlns="http://www.w3.org/2000/svg" width="400" height="300" viewBox="0 0 400 300">
+            <g id="circle">
+                <circle cx="100" cy="100" r="50" style="stroke-width: 5; stroke: #000" fill="none" />
+            </g>
+
+            <!-- translate(x-value, y-value) -->
+            <use xlink:href="#circle" transform="translate(100, 0)" />
+            <!-- 简写 -->
+            <use xlink:href="#circle" x="50" y="50" />
+        </svg>
+    </main>
+</template>
+
+<script>
+    module.exports = {}
+</script>
