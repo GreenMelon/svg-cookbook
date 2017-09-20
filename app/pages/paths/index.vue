f539e689e46858d1347e242144776ebb8a2fb025
caigua <caigua@huanleguang.com>
2017-09-20 11:55:19 +0800

    Paths - moveto,lineto,closepath

---
+++ app/pages/paths/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <main>
+        <ul class="mod-categories list-category">
+            <li v-for="category in categories" class="item-main">
+                <h2 v-text="category.name"></h2>
+                <ul class="list-instance">
+                    <li v-for="instance in category.instances" class="item-sub">
+                        <router-link :to="instance.route">{{ instance.name }}
+                        </router-link>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+    module.exports = {
+        data() {
+            return {
+                categories: [
+                    {
+                        name: 'moveTo',
+                        instances: [
+                            {
+                                name: '001',
+                                route: '/paths/moveto/001'
+                            }
+                        ]
+                    },{
+                        name: 'closepath',
+                        instances: [
+                            {
+                                name: '001',
+                                route: '/paths/closepath/001'
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        methods: {},
+        created() {}
+    }
+</script>
