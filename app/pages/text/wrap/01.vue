712fea533cac01a46b1adb25a015effbfa7922ba
caigua <caigua@huanleguang.com>
2018-03-16 22:52:25 +0800

    Text - wrap

--- app/pages/text/wrap/01.vue
+++ app/pages/text/wrap/01.vue
@@ -1,9 +1,3 @@
-<style scoped="">
-    span {
-        font-size: 12px;
-    }
-</style>
-
 <template>
     <main>
         <span>文字换行显示</span>
@@ -21,6 +15,16 @@
             <foreignObject width="120" height="50">
                 <body xmlns="http://www.w3.org/1999/xhtml" style="margin: 0; padding: 0; background: transparent;">
                     <p style="margin: 0; font-size: 12px;">一段需要word wrap的文字。</p>
+                </body>
+            </foreignObject>
+        </svg>
+
+        <!-- foreignObject 支持br -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="150" viewBox="0 0 200 150">
+            <foreignObject width="120" height="50">
+                <body xmlns="http://www.w3.org/1999/xhtml" style="margin: 0; padding: 0; background: transparent;">
+                    <span>xx<br>yy</span>
+                    <div>x&nbsp;&nbsp;&nbsp;y</div>
                 </body>
             </foreignObject>
         </svg>
