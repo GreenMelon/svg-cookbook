4467542951b50655018b1ee43648901e2660b0d4
caigua <caigua@huanleguang.com>
2018-10-02 16:39:44 +0800

    Case - XML2String && String2XML

--- app/pages/case/other/serialize-svg.vue
+++ app/pages/case/other/serialize-svg.vue
@@ -3,7 +3,8 @@
 
 <template>
     <main>
-        <button @click="parseSVG">解析 SVG</button>
+        <button @click="XML2String">XML2String</button>
+        <button @click="String2XML">String2XML</button>
     </main>
 </template>
 
@@ -11,17 +12,34 @@
     import $ from 'jquery';
 
     export default {
+        data() {
+            return {
+                svg: {},
+                svgContent: '',
+            }
+        },
         methods: {
-            parseSVG: function() {
+            XML2String() {
                 $.getJSON('./data/svg.json').then(function(element) {
                     $.get('./data/delete.svg').then(function(svg) {
                         const parser = new XMLSerializer();
                         const svgContent = parser.serializeToString(svg);
-                        eelementl.content = svgContent;
+                        element.content = svgContent;
 
-                        console.log({ svgContent });
+                        console.log({ svg, svgContent });
+
+                        this.svg = svg;
+                        this.svgContent = svgContent;
                     });
                 });
+            },
+            String2XML() {
+                const { svgContent } = this;
+
+                const parser = new DOMParser();
+                const xmlObject = parser.parseFromString(svgContent, 'text/xml');
+
+                console.log({ xmlObject });
             },
         },
     }
