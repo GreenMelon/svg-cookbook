8f36f6b5d1f383571b962035fc0c934937bb3c97
caigua <caigua@huanleguang.com>
2019-08-01 11:55:40 +0800

    interactivity

---
+++ app/pages/interactivity/user-trigger/01.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <svg xmlns="http://www.w3.org/2000/svg">
+            <circle
+                cx="60"
+                cy="60"
+                r="60"
+                style="fill: #007dd4;"
+            >
+                <animate
+                    attributeName="cx"
+                    attributeType="XML"
+                    to="160"
+                    begin="click"
+                    dur="4s"
+                    fill="freeze"
+                    repeatCount="indefinite"
+                />
+                <animate
+                    attributeName="fill"
+                    attributeType="XML"
+                    to="#ed143d"
+                    begin="click"
+                    dur="4s"
+                    fill="freeze"
+                    repeatCount="indefinite"
+                />
+            </circle>
+        </svg>
+    </main>
+</template>
+
+<script>
+    export default {}
+</script>
